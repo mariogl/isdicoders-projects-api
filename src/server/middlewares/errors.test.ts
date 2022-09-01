0c9c69df22fa5bb8e6ee5bc5ce8e51920720ef5e
Mario González <mario@mariogl.com>
2022-09-01 19:24:02 +0200

    Tested corner case for middleware error

--- src/server/middlewares/errors.test.ts
+++ src/server/middlewares/errors.test.ts
@@ -17,19 +17,46 @@ describe("Given a notFoundError middleware", () => {
 });
 
 describe("Given a generalError middleware", () => {
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
+  } as Partial<Response>;
+
   describe("When it receives a 'Test error' error with status 405 and a res function", () => {
     const status = 405;
     const message = "Test error";
-    const error = new CustomError(status, message, "");
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    } as Partial<Response>;
+    const error = new CustomError(status, "", message);
 
     test("Then it should call res' status method with 405", () => {
       generalError(error, null, res as Response, null);
 
       expect(res.status).toHaveBeenCalledWith(status);
+    });
+
+    test("Then it should call res' json method with error 'Test error'", () => {
+      generalError(error, null, res as Response, null);
+
+      expect(res.json).toHaveBeenCalledWith({ error: message });
+    });
+  });
+
+  describe("When it receives a error without status and public message", () => {
+    const error = new Error();
+
+    test("Then it should call res' status method with 500", () => {
+      const status = 500;
+
+      generalError(error as CustomError, null, res as Response, null);
+
+      expect(res.status).toHaveBeenCalledWith(status);
+    });
+
+    test("Then it should call res' json method with error 'Server error'", () => {
+      const message = "Server error";
+
+      generalError(error as CustomError, null, res as Response, null);
+
+      expect(res.json).toHaveBeenCalledWith({ error: message });
     });
   });
 });
